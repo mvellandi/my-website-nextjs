9f93ea8407b695d81ac418b2c46a95d5196d3aa4
Mario Vellandi <mvellandi@hotmail.com>
2022-11-30 11:35:36 +0100

    space adjustments

--- components/main/Items.js
+++ components/main/Items.js
@@ -4,8 +4,8 @@ export default function Items({ data: { type, route, value: items }, as }) {
   const Component = as || "div";
   return (
     // ITEMS: left-aligned children, full width until large screen
-    <Component className="flex justify-center w-full p-5 bg-white">
-      <ul className="w-full max-w-screen-lg grid grid-cols-1 gap-5 md:gap-6 gap-x-7 md:grid-cols-2 md:min-w-[620px] justify-center">
+    <Component className="flex justify-center w-full pt-5 pb-9 md:py-9 px-5 bg-white">
+      <ul className="w-full max-w-screen-lg grid grid-cols-1 gap-6 md:grid-cols-2 md:gap-x-12 md:min-w-[620px] justify-center">
         {items.map((item) => {
           return <Card as="li" key={item._id} route={route} data={item} />;
         })}
