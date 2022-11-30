a14ebe6977968253ca7b189dd1aa4459771effd1
Mario Vellandi <mvellandi@hotmail.com>
2022-11-30 12:51:11 +0100

    + new phone breakpoint, sm tablet adjust

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,23 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      pmd: "388px",
+      sm: "600px",
+      // => @media (min-width: 600px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
     colors: {
       current: "currentColor",
       transparent: "transparent",
