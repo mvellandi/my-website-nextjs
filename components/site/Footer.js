c6c4d347e1c67ac10f22a690d50ab84fb04f35d2
Mario Vellandi <mvellandi@hotmail.com>
2022-12-04 22:27:52 +0100

    extracted shared x-padding/breakpoints to a class

--- components/site/Footer.js
+++ components/site/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="flex justify-center items-center w-full h-12 border-t-5 border-t-red-shade bg-red lg:h-32">
-      <div className="w-full max-w-screen-lg px-5 text-white">
+      <div className="w-full max-w-screen-lg site-padding-x text-white">
         <span className="text-sm">
           &copy; {new Date().getFullYear()} - Mario Vellandi
         </span>
