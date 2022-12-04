0648da7edad0ac94d4d681d5ccc50d8905b25cdc
Mario Vellandi <mvellandi@hotmail.com>
2022-12-04 22:30:23 +0100

    modified header width options based on layout prop

--- components/site/Header.js
+++ components/site/Header.js
@@ -1,12 +1,23 @@
+import cn from "classnames";
 import Link from "next/link";
 
-export default function Header({ as }) {
+export default function Header({ as, layout }) {
   const Component = as || "div";
+  let headAttributes = "flex justify-between items-center w-full";
+  switch (layout) {
+    case "main":
+      headAttributes = cn(headAttributes, "max-w-screen-xl");
+      break;
+    case "item":
+      headAttributes = cn(headAttributes, "max-w-[870px] xl:max-w-screen-xl");
+    default:
+      break;
+  }
   return (
     // HEADER BACKGROUND ROW: full width, h-centered child
-    <Component className="flex justify-center w-full bg-red py-2 sm:pt-6 fixed z-10 h-[56px] sm:h-[72px] lg:h-[80px]">
+    <Component className="flex justify-center w-full bg-red site-padding-x py-2 sm:pt-6 fixed z-10 h-[56px] sm:h-[72px] lg:h-[80px]">
       {/* HEADER CONTENT ROW: full width until large screen, h-centered children (logo and nav), space-between  */}
-      <div className="flex justify-between items-center w-full max-w-screen-lg px-5">
+      <div className={headAttributes}>
         {/* LOGO */}
         <span className="inline text-3xl lg:text-4xl font-brand text-white drop-shadow">
           <Link href="/">Vellandi</Link>
