4a00976c51c0131241dff866c8d89d2625bd3e8d
Mario Vellandi <mvellandi@hotmail.com>
2022-12-04 22:24:15 +0100

    dry refactored for improved desktop layout

--- components/project/Item.js
+++ components/project/Item.js
@@ -2,10 +2,11 @@
 import { urlForImage } from "/lib/sanity";
 import Link from "next/link";
 import { PortableText } from "@portabletext/react";
+import { useState } from "react";
 
 export default function Item({ data, as }) {
   const Component = as || "div";
-  const {
+  let {
     name,
     sector,
     coverImage,
@@ -17,10 +18,10 @@ export default function Item({ data, as }) {
     media,
   } = data;
   return (
-    <div className="flex flex-col items-center bg-white">
-      <div className="flex w-full max-w-screen-lg">
-        <Component className="flex flex-col gap-10 md:gap-14 bg-white w-full max-w-[870px] px-5 pt-[50px] lg:pt-[80px] pb-10 lg:pb-[100px]">
-          <div className="flex justify-between sm:justify-start sm:gap-9">
+    <div className="flex flex-col items-center bg-white site-padding-x">
+      <div className="flex w-full max-w-[870px] xl:max-w-screen-xl">
+        <Component className="flex flex-col gap-10 md:gap-14 bg-white w-full max-w-[870px] xl:max-w-screen-xl xl:flex-none pt-[50px] lg:pt-[80px] pb-10 lg:pb-[100px]">
+          <div className="w-full flex justify-between sm:justify-start sm:gap-9">
             <div className="flex flex-col gap-2 sm:order-2">
               <h1 className="text-3xl text-black font-light -tracking-1 leading-tight">
                 {name}
@@ -37,93 +38,116 @@ export default function Item({ data, as }) {
               className="w-[90px] h-[90px] lg:w-[100px] lg:h-[100px] order-1"
             />
           </div>
-          {summary && (
-            <section>
-              <h3 className="text-xl text-orange font-bold mb-2">Summary</h3>
-              <div className="prose-lg lg:prose-xl">
-                <PortableText value={summary} />
-              </div>
-            </section>
-          )}
-          {features && (
-            <section>
-              <h3 className="text-xl text-orange font-bold mb-2">Features</h3>
-              <div className="prose-lg lg:prose-xl">
-                <PortableText value={features} onMissingComponent={false} />
-              </div>
-            </section>
-          )}
-          <div className="flex flex-col gap-10 md:grid md:grid-cols-2 md:gap-5">
-            {structure && (
-              <section className="pr-5">
-                <h3 className="text-xl text-orange font-bold mb-2">
-                  Tech / Design
-                </h3>
-                <div className="flex flex-col lg:text-lg space-y-2">
-                  {structure.map(({ aspect, values }) => (
-                    <div key={aspect} className="grid grid-cols-[100px_1fr]">
-                      <h4 className="inline">{aspect}:</h4>
-                      <ul className="inline-flex">
-                        {values.map((name, idx) => (
-                          <li key={name}>
-                            {name}
-                            {idx < values.length - 1 && <span>,&nbsp;</span>}
-                          </li>
-                        ))}
-                      </ul>
+          <div className="grid grid-col gap-10 md:gap-14 xl:grid xl:grid-cols-3 xl:gap-16">
+            <div className="xl:col-span-2 xl:max-w-[780px]">
+              {summary && (
+                <section>
+                  <h3 className="text-xl text-orange font-bold mb-2">
+                    Summary
+                  </h3>
+                  <div className="prose-lg lg:prose-xl">
+                    <PortableText value={summary} />
+                  </div>
+                </section>
+              )}
+            </div>
+            <div className="row-span-2 flex flex-col gap-10 md:gap-14">
+              {features && (
+                <section>
+                  <h3 className="text-xl text-orange font-bold mb-2">
+                    Features
+                  </h3>
+                  <div className="prose-lg lg:prose-xl">
+                    <PortableText value={features} onMissingComponent={false} />
+                  </div>
+                </section>
+              )}
+              <div className="flex flex-col gap-10 md:grid md:grid-cols-2 md:gap-5 xl:flex xl:flex-col xl:gap-14">
+                {structure && (
+                  <section className="pr-5 xl:pr-0">
+                    <h3 className="text-xl text-orange font-bold mb-2">
+                      Tech / Design
+                    </h3>
+                    <div className="flex flex-col lg:text-lg space-y-2">
+                      {structure.map(({ aspect, values }) => (
+                        <div
+                          key={aspect}
+                          className="grid grid-cols-[100px_1fr]"
+                        >
+                          <h4 className="inline">{aspect}:</h4>
+                          <ul className="inline-flex">
+                            {values.map((name, idx) => (
+                              <li key={name}>
+                                {name}
+                                {idx < values.length - 1 && (
+                                  <span>,&nbsp;</span>
+                                )}
+                              </li>
+                            ))}
+                          </ul>
+                        </div>
+                      ))}
                     </div>
-                  ))}
-                </div>
-              </section>
-            )}
-            {links && (
-              <section>
-                <h3 className="text-xl text-orange font-bold mb-2">
-                  Project Links
-                </h3>
-                <ul className="space-y-2 lg:text-lg">
-                  {links.map(({ _key, text, url }) => (
-                    <li key={_key} className="text-link">
-                      <Link href={url}>{text}</Link>
-                    </li>
-                  ))}
-                </ul>
-              </section>
-            )}
-          </div>
-          {process && (
-            <section>
-              <h3 className="text-xl text-orange font-bold mb-2">Process</h3>
-              <div className="prose-lg lg:prose-xl">
-                {process.map(({ _key, _type, ...rest }) => {
-                  switch (_type) {
-                    case "richText":
-                      return (
-                        <PortableText
-                          value={rest.body}
-                          onMissingComponent={false}
-                        />
-                      );
-                      break;
-                    case "headingRichText":
-                      return (
-                        <>
-                          <h4 className="font-bold">{rest.heading}</h4>
+                  </section>
+                )}
+                {links && (
+                  <section>
+                    <h3 className="text-xl text-orange font-bold mb-2">
+                      Project Links
+                    </h3>
+                    <ul className="space-y-2 lg:text-lg">
+                      {links.map(({ _key, text, url }) => (
+                        <li key={_key} className="text-link">
+                          <Link href={url}>{text}</Link>
+                        </li>
+                      ))}
+                    </ul>
+                  </section>
+                )}
+              </div>
+            </div>
+            {process && (
+              <section className="max-w-[780px] xl:col-span-2">
+                <h3 className="text-xl text-orange font-bold mb-2">Process</h3>
+                <div className="prose-lg lg:prose-xl">
+                  {process.map(({ _key, _type, ...rest }) => {
+                    switch (_type) {
+                      case "richText":
+                        return (
                           <PortableText
                             value={rest.body}
                             onMissingComponent={false}
                           />
-                        </>
-                      );
-                  }
-                })}
-              </div>
-            </section>
-          )}
+                        );
+                      case "headingRichText":
+                        return (
+                          <>
+                            <h4 className="font-bold">{rest.heading}</h4>
+                            <PortableText
+                              value={rest.body}
+                              onMissingComponent={false}
+                            />
+                          </>
+                        );
+                    }
+                  })}
+                </div>
+              </section>
+            )}
+          </div>
           {media && (
             <section>
               <h3 className="text-xl text-orange font-bold mb-2">Media</h3>
-              <div className="flex flex-col gap-10 md:grid md:grid-cols-2 md:gap-5">
+              <div className="grid gap-10 sm:justify-center md:grid-cols-2 md:gap-5 xl:grid-cols-3 xl:gap-16 bord-blue">
+                {media.map(({ _key, ...rest }) => (
+                  <div key={_key}>
+                    <img
+                      src={urlForImage(rest.image).url()}
+                      alt="rest.alt"
+                      className="w-full max-w-[420px]"
+                    />
+                  </div>
+                ))}
                 {media.map(({ _key, ...rest }) => (
                   <div key={_key}>
                     <img
