3a99170f0071c3fcbc4b8bcd13011e794205eb25
Mario Vellandi <mvellandi@hotmail.com>
2022-12-05 21:41:59 +0100

    rwd + padding adjustments

--- components/main/Hero.js
+++ components/main/Hero.js
@@ -11,21 +11,21 @@ export default function Hero({ as }) {
   const Component = as || "div";
   return (
     // SECTION BACKGROUND + CONTENT ROW: full-width, h-centered children
-    <Component className="position-adjust flex justify-center w-full gap-5 lg:gap-12 px-5 pt-4 pb-6 lg:py-8 bg-red text-white border-b-8 border-red-shade">
+    <Component className="position-adjust flex justify-center w-full gap-4 sm:gap-12 site-padding-x pt-4 pb-6 lg:py-8 bg-red text-white border-b-8 border-red-shade">
       {/* TEXT COLUMN */}
       <div className="flex flex-col justify-center gap-6 md:w-[380px] lg:gap-12">
-        <h1 className="text-sm md:text-md lg:text-xl text-white order-2">
+        <h1 className="text-sm sm:text-md md:text-lg lg:text-xl text-white order-2">
           {"\u2192"}{" "}
           <span className="about-mario">
             <a href="/about">about Mario Vellandi</a>
           </span>
         </h1>
-        <h2 className="text-xl pmd:text-2xl -tracking-3 lg:text-3xl lg:leading-[1.22] font-light drop-shadow order-1">
+        <h2 className="text-xl pmd:text-2xl -tracking-3 md:text-3xl md:leading-[1.22] font-light drop-shadow order-1">
           I research and produce digital media
         </h2>
       </div>
       {/* PHOTO */}
-      <div className="min-w-[126px] pmd:min-w-[140px] md:w-[200px]">
+      <div className="min-w-[112px] pmd:min-w-[140px] md:w-[200px]">
         {/* <div sty> */}
         <Image src={portrait} style={imageStyle} alt="photo: mario vellandi" />
       </div>
--- components/main/Items.js
+++ components/main/Items.js
@@ -4,7 +4,7 @@ export default function Items({ data: { type, route, value: items }, as }) {
   const Component = as || "div";
   return (
     // ITEMS: left-aligned children, full width until large screen
-    <Component className="flex justify-center w-full pt-5 pb-9 md:py-9 px-5 bg-white">
+    <Component className="flex justify-center w-full site-padding-x pt-5 pb-9 md:py-9 bg-white">
       <ul className="w-full max-w-screen-lg grid grid-cols-1 gap-6 md:grid-cols-2 md:gap-x-12 md:min-w-[620px] justify-center">
         {items.map((item) => {
           return <Card as="li" key={item._id} route={route} data={item} />;
