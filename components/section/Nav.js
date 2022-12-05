ca94e6996717e3a1000c0bf42560a36f84e59820
Mario Vellandi <mvellandi@hotmail.com>
2022-12-05 21:41:15 +0100

    rwd adjustment; added item type to next/prev links

--- components/section/Nav.js
+++ components/section/Nav.js
@@ -3,7 +3,8 @@ import Link from "next/link";
 
 export default function SectionNav({ place, as, title, prev, next, path }) {
   const Component = as || "div";
-  let navAttributes = "flex justify-between w-full max-w-[870px] px-5";
+  let navAttributes =
+    "flex justify-between w-full max-w-[870px] xl:max-w-screen-xl";
   switch (place) {
     case "top":
       navAttributes = cn(navAttributes, "py-4 lg:pt-11");
@@ -17,8 +18,8 @@ export default function SectionNav({ place, as, title, prev, next, path }) {
   const sectionHomeRoute = path === "/projects" ? "/" : path;
   return (
     // NAV BACKGROUND
-    <Component className="flex justify-center w-full border-b border-gray-400 bg-gray-25 lg:border-b-2">
-      <div className="w-full max-w-screen-lg">
+    <Component className="flex justify-center w-full site-padding-x border-b border-gray-400 bg-gray-25 lg:border-b-2">
+      <div className="w-full max-w-[870px] xl:max-w-screen-xl">
         <div className={navAttributes}>
           <Link href={sectionHomeRoute}>
             <a className="text-lg font-medium lg:text-xl">{title}</a>
@@ -26,13 +27,18 @@ export default function SectionNav({ place, as, title, prev, next, path }) {
           <div className="flex gap-4 font-semibold text-base lg:text-lg">
             {prev && (
               <Link href={`${path}/${prev}`}>
-                <a className="text-link">previous</a>
+                <a className="text-link">
+                  previous
+                  <span className="hidden md:inline">&nbsp;project</span>
+                </a>
               </Link>
             )}
             {prev && next && <span className="font-normal">|</span>}
             {next && (
               <Link href={`${path}/${next}`}>
-                <a className="text-link">next</a>
+                <a className="text-link">
+                  next<span className="hidden md:inline">&nbsp;project</span>
+                </a>
               </Link>
             )}
           </div>
