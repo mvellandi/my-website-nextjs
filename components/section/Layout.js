1e43166a09559b3c49697ef8104c6a232fff3cfa
Mario Vellandi <mvellandi@hotmail.com>
2022-12-06 10:32:58 +0100

    removed log

--- components/section/Layout.js
+++ components/section/Layout.js
@@ -5,30 +5,29 @@ import SectionNav from "/components/section/Nav";
 import Footer from "/components/site/Footer";
 
 export default function Layout({ nav, children }) {
-  console.log("nav", nav);
   return (
-      <Container>
-        <SiteHeader as="header" layout="item" />
-        <main className="position-adjust flex flex-col justify-center w-full">
-          <SectionNav
-            as="nav"
-            place="top"
-            title={nav.title}
-            path={nav.path}
-            prev={nav.prev}
-            next={nav.next}
-          />
-          {children}
-          <SectionNav
-            as="nav"
-            place="bottom"
-            title={nav.title}
-            path={nav.path}
-            prev={nav.prev}
-            next={nav.next}
-          />
-        </main>
-        <Footer />
-      </Container>
+    <Container>
+      <SiteHeader as="header" layout="item" />
+      <main className="position-adjust flex flex-col justify-center w-full">
+        <SectionNav
+          as="nav"
+          place="top"
+          title={nav.title}
+          path={nav.path}
+          prev={nav.prev}
+          next={nav.next}
+        />
+        {children}
+        <SectionNav
+          as="nav"
+          place="bottom"
+          title={nav.title}
+          path={nav.path}
+          prev={nav.prev}
+          next={nav.next}
+        />
+      </main>
+      <Footer />
+    </Container>
   );
 }
