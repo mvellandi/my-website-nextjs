72418116c2bd3ede54e87a8b0bcc4497be5efcb9
Mario Vellandi <mvellandi@hotmail.com>
2022-12-08 13:59:16 +0100

    +xl breakpoint styling if prop "type" is project

--- components/section/Nav.js
+++ components/section/Nav.js
@@ -12,8 +12,7 @@ export default function SectionNav({
   path,
 }) {
   const Component = as || "div";
-  let navAttributes =
-    "flex justify-between w-full max-w-[870px] xl:max-w-screen-xl py-4";
+  let navAttributes = "flex justify-between w-full max-w-[870px] py-4";
   switch (place) {
     case "top":
       navAttributes = cn(navAttributes, "lg:pt-11");
@@ -23,14 +22,23 @@ export default function SectionNav({
     default:
       break;
   }
-  console.log("type", type);
+  if (type === "project")
+    navAttributes = cn(navAttributes, "xl:max-w-screen-xl ");
   // Set homepage as index for projects
   const sectionHomeRoute = path === "/projects" ? "/" : path;
   return (
     // NAV BACKGROUND
     <Component className="flex justify-center w-full site-padding-x border-b border-gray-400 bg-gray-25 lg:border-b-2">
-      <div className="w-full max-w-[870px] xl:max-w-screen-xl">
-        <div className={navAttributes}>
+      <div
+        className={`w-full max-w-[870px] ${
+          type === "project" && "xl:max-w-screen-xl"
+        }`}
+      >
+        <div
+          className={`${navAttributes} ${
+            type === "project" && "xl:max-w-screen-xl"
+          }`}
+        >
           <Link href={sectionHomeRoute}>
             <a className="text-lg font-medium lg:text-xl">
               <RiArrowLeftSLine className="inline mb-1" />
