0e42c4f488080c8ab4259f3d3023f61c3e0ebe08
Mario Vellandi <mvellandi@hotmail.com>
2022-12-08 17:54:09 +0100

    + 1280 breakpoint with 3 columns

--- components/main/Items.js
+++ components/main/Items.js
@@ -5,7 +5,7 @@ export default function Items({ data: { type, route, value: items }, as }) {
   return (
     // ITEMS: left-aligned children, full width until large screen
     <Component className="flex justify-center w-full site-padding-x pt-5 pb-9 md:py-9 bg-white">
-      <ul className="w-full max-w-screen-lg grid grid-cols-1 gap-6 md:grid-cols-2 md:gap-x-12 md:min-w-[620px] justify-center">
+      <ul className="w-full max-w-screen-xl justify-center grid grid-cols-1 gap-6 md:grid-cols-2 md:gap-x-12 md:min-w-[620px] xl:grid-cols-3 xl:gap-y-10">
         {items.map((item) => {
           return <Card as="li" key={item._id} route={route} data={item} />;
         })}
