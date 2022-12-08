7b2ed92b14fe4893f4a803395473c02cabf34ac4
Mario Vellandi <mvellandi@hotmail.com>
2022-12-08 12:07:59 +0100

    init commit

---
+++ components/article/Item.js
@@ -0,0 +1,29 @@
+/* eslint-disable @next/next/no-img-element */
+import { urlForImage } from "/lib/sanity";
+import Link from "next/link";
+import { PortableText } from "@portabletext/react";
+
+export default function Item({ data, as }) {
+  const Component = as || "div";
+  const { headline, body } = data;
+
+  return (
+    <div className="flex flex-col items-center bg-white site-padding-x">
+      <div className="flex w-full max-w-[870px] xl:max-w-screen-xl">
+        <Component className="flex flex-col gap-10 md:gap-14 bg-white w-full max-w-[870px] xl:max-w-screen-xl xl:flex-none pt-[50px] lg:pt-[80px] pb-[60px] lg:pb-[100px]">
+          <h1 className="text-3xl text-black font-light -tracking-1 leading-tight">
+            {headline}
+          </h1>
+          <div className="prose-lg lg:prose-xl">
+            {(() => {
+              const [_type, content] = [body._type, body.body];
+              return (
+                <PortableText value={content} onMissingComponent={false} />
+              );
+            })()}
+          </div>
+        </Component>
+      </div>
+    </div>
+  );
+}
