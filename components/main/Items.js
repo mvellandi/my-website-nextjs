0eed244cc2a0e03ea17b0cf71dcdab85987b3943
Mario Vellandi <mvellandi@hotmail.com>
2022-12-15 23:45:34 +0100

    mod grid padding

--- components/main/Items.js
+++ components/main/Items.js
@@ -5,7 +5,7 @@ export default function Items({ data: { type, route, value: items }, as }) {
   return (
     // ITEMS: left-aligned children, full width until large screen
     <Component className="flex justify-center w-full site-padding-x pt-5 pb-9 md:py-9 bg-white">
-      <ul className="w-full max-w-screen-xl justify-center grid grid-cols-1 gap-6 md:grid-cols-2 md:gap-x-12 md:min-w-[620px] xl:grid-cols-3 xl:gap-y-10">
+      <ul className="w-full max-w-screen-xl justify-center grid grid-cols-1 gap-6 md:grid-cols-2 md:gap-12 md:min-w-[620px] xl:grid-cols-3 3xl:gap-9 2k:gap-16">
         {items.map((item) => {
           return <Card as="li" key={item._id} route={route} data={item} />;
         })}
