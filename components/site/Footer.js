16942d20f40ba7d25e6144e56255c19e43cb5323
Mario Vellandi <mvellandi@hotmail.com>
2022-12-31 00:30:13 +0100

    testing button sizes

--- components/site/Footer.js
+++ components/site/Footer.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
     <footer className="flex justify-center items-start w-full h-full py-16 border-t-5 border-t-red-shade bg-red">
@@ -5,7 +7,56 @@ export default function Footer() {
         <span className="text-sm xl:text-base">
           &copy; {new Date().getFullYear()} - Mario Vellandi
         </span>
-        
+        <div className="space-y-12 hidden">
+          <div>
+            <button className="btn btn-xs btn-primary-bright">menu xs</button>
+          </div>
+          <div>
+            <button className="btn btn-xs-round btn-primary-bright">
+              menu xs
+            </button>
+          </div>
+          <div>
+            <button className="btn btn-sm btn-primary-bright">menu sm</button>
+          </div>
+          <div>
+            <button className="btn btn-sm-round btn-primary-bright">
+              menu sm
+            </button>
+          </div>
+          <div>
+            <button className="btn btn-md btn-primary-bright">menu md</button>
+          </div>
+          <div>
+            <button className="btn btn-md-round btn-primary-bright">
+              menu md
+            </button>
+          </div>
+          <div>
+            <button className="btn btn-lg btn-primary-bright">menu lg</button>
+          </div>
+          <div>
+            <button className="btn btn-lg-round btn-primary-bright">
+              menu lg
+            </button>
+          </div>
+          <div>
+            <button className="btn btn-xl btn-primary-bright">menu xl</button>
+          </div>
+          <div>
+            <button className="btn btn-xl-round btn-primary-bright">
+              menu xl
+            </button>
+          </div>
+          <div>
+            <button className="btn btn-2xl btn-primary-bright">menu 2xl</button>
+          </div>
+          <div>
+            <button className="btn btn-2xl-round btn-primary-bright">
+              menu 2xl
+            </button>
+          </div>
+        </div>
       </div>
     </footer>
   );
