5f5dbb7a2c61165c39d15e473cacdea4b3cc6be2
Mario Vellandi <mvellandi@hotmail.com>
2022-12-31 00:27:38 +0100

    code reordering

--- components/main/Hero.js
+++ components/main/Hero.js
@@ -15,13 +15,12 @@ export default function Hero({ as }) {
     <Component className="main-content-y flex justify-center w-full gap-16 site-padding-x pt-16 pb-24 sm:gap-48 md:pb-32 md:pt-8 lg:pt-0 lgtall:pt-20 lgtall:pb-40 2k:pb-48 2k:pt-16 bg-red text-white border-b-8 border-red-shade">
       {/* TEXT COLUMN */}
       <div className="flex flex-col justify-center gap-24 md:w-[380px] lg:w-[440px] lg:gap-48">
-        <h1 className="relative text-sm sm:text-lg lg:text-xl text-white order-2">
-          {"\u2192"}{" "}
-          <Link className="about-mario" href="/about">
-            about Mario Vellandi
+        <h1 className="relative w-fit order-2">
+          <Link className="target main-about" href="/about">
+            <span aria-hidden>{"\u2192"}{" "}</span><span className="main-about-text">about Mario Vellandi</span>
           </Link>
         </h1>
-        <h2 className="text-xl pmd:text-2xl -tracking-3 md:text-3xl md:leading-[1.22] lgtall:text-[4.2rem] font-light drop-shadow order-1">
+        <h2 className="text-xl -tracking-3 font-light drop-shadow order-1 pmd:text-2xl md:text-3xl md:leading-[1.22] lgtall:text-[4.2rem]">
           I research and produce digital media
         </h2>
       </div>
