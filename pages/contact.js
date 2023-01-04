dfc7872ce909e77f8d5362f422e0ec32fe91f570
Mario Vellandi <mvellandi@hotmail.com>
2023-01-04 11:30:41 +0100

    ic

---
+++ pages/contact.js
@@ -0,0 +1,19 @@
+import PageLayout from "/components/page/Layout";
+
+export default function Contact({ preview = false }) {
+  return (
+    <PageLayout>
+      <h1 className="text-3xl text-black font-light -tracking-1 leading-tight">
+        Contact
+      </h1>
+    </PageLayout>
+  );
+}
+
+export async function getStaticProps({ params, preview = false }) {
+  return {
+    props: {
+      preview,
+    },
+  };
+}
