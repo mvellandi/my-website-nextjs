5eebf9d43950ce8f0d2b2f6499c9b746e2b0ad1d
Mario Vellandi <mvellandi@hotmail.com>
2023-01-05 10:59:45 +0100

    ~ Renamed component from generic to specific - Nonfunctional Container el - SiteHeader 'as' prop to default to div for a11y reasons + HeaderOffset div for fixed position SiteHeader + Notes, todos ~ Replaced nonfunctional main with ContentColumn el

--- components/page/Layout.js
+++ components/page/Layout.js
@@ -1,16 +1,16 @@
-// import Footer from '/components/site/Footer'
-import Container from "/components/site/Container";
 import SiteHeader from "/components/site/Header";
+import HeaderOffset from "/components/elements/HeaderOffset";
+import ContentColumn from "/components/elements/ContentColumn";
 import Footer from "/components/site/Footer";
 
-export default function Layout({ children }) {
+// PAGES ARE FOR SINGLE-RECORD RESOURCES (e.g. /about) AND FOR STATIC PAGES (e.g. /contact)
+export default function PageLayout({ children }) {
   return (
-    <Container>
-      <SiteHeader as="header" type="page" />
-      <main className="item-content-y flex flex-col justify-center w-full">
-        {children}
-      </main>
+    <>
+      <SiteHeader type="page" />
+      <HeaderOffset type="page" />
+      <ContentColumn>{children}</ContentColumn>
       <Footer />
-    </Container>
+    </>
   );
 }
