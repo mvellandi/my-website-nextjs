6898f8aeb6b93ec9e0a91577327a25400a02bc6e
Mario Vellandi <mvellandi@hotmail.com>
2023-01-05 11:42:04 +0100

    + TODO note + Items (main content) element for consistency among all site static and dynamic pages

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,17 @@
 import Meta from "/components/site/Meta";
 import MainLayout from "/components/main/Layout";
+import Items from "/components/main/Items";
 import { getAllProjectCards } from "/lib/project";
+
+// TODO: Change the layout to list all section items, in 3 separate lists
 
 export default function Home({ data, preview }) {
   return (
     <>
       <Meta data={null} />
-      <MainLayout preview={preview} data={data} />
+      <MainLayout preview={preview} data={data}>
+        <Items as="main" data={data} />
+      </MainLayout>
     </>
   );
 }
