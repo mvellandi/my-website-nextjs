d4bf1817b5602c44b4d9169e43ce0b9cc5a96cdb
Mario Vellandi <mvellandi@hotmail.com>
2023-01-05 15:42:52 +0100

    ~ Duplicated homepage for now, as it's the same content

--- pages/projects.js
+++ pages/projects.js
@@ -1,1 +1,28 @@
-// TODO: Create a projects index page
+import Meta from "/components/site/Meta";
+import MainLayout from "/components/main/Layout";
+import Items from "/components/main/Items";
+import { getAllProjectCards } from "/lib/project";
+
+// TODO: Change the layout to list all section items, in 3 separate lists
+
+export default function Projects({ data, preview }) {
+  return (
+    <>
+      <Meta data={null} />
+      <MainLayout preview={preview} data={data}>
+        <Items as="main" data={data} />
+      </MainLayout>
+    </>
+  );
+}
+
+export async function getStaticProps({ preview = false }) {
+  const data = await getAllProjectCards({ preview });
+  return {
+    props: {
+      data,
+      preview,
+    },
+    revalidate: 1,
+  };
+}
