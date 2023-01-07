3e3e11a005efcf5c9c0dbee314ef21dc336a7936
Mario Vellandi <mvellandi@hotmail.com>
2023-01-07 23:40:00 +0100

    ~ Use element content width from imported global constant ~ Refactor for simplicity ~ Rename variables for clarity and local scope use ~ Increase top padding and move border to top, when nav is on page bottom ~ Move arrow and page title further beyond left margin + Add aria-hidden to arrow symbol

--- components/section/Nav.js
+++ components/section/Nav.js
@@ -1,6 +1,11 @@
 import cn from "classnames";
 import Link from "next/link";
 import { RiArrowLeftSLine } from "react-icons/ri";
+import { elementContentWidthStyle as navContentWidthStyle } from "/components/site/constants";
+
+const pageTypeCheck = (type, list) => {
+  return list.includes(type);
+};
 
 export default function SectionNav({
   place,
@@ -13,18 +18,17 @@ export default function SectionNav({
 }) {
   const Component = as || "div";
 
-  let navAttributes =
-    "flex justify-between items-center w-full max-w-[870px] py-12";
+  let navStyle =
+    "flex justify-center w-full site-padding-x border-b border-gray-400 bg-gray-25";
+  let navContentStyle = `flex justify-between items-center w-full py-12 ${navContentWidthStyle[type]}`;
   switch (place) {
     case "top":
-      navAttributes = cn(navAttributes, "lg:pt-44");
+      navStyle = cn(navStyle, "lg:border-b-2");
+      navContentStyle = cn(navContentStyle, "lg:pt-44");
       break;
     case "bottom":
-      navAttributes = cn(navAttributes, "lg:pb-44");
-  }
-
-  if (type === "project") {
-    navAttributes = cn(navAttributes, "xl:max-w-screen-xl ");
+      navStyle = cn(navStyle, "lg:border-t-2");
+      navContentStyle = cn(navContentStyle, "lg:pb-44 lg:pt-24");
   }
 
   // Set homepage as index for projects
@@ -32,38 +36,28 @@ export default function SectionNav({
 
   return (
     // NAV BACKGROUND
-    <Component className="flex justify-center w-full site-padding-x border-b border-gray-400 bg-gray-25 lg:border-b-2">
-      <div
-        className={`w-full max-w-[870px] ${
-          type === "project" && "xl:max-w-screen-xl"
-        }`}
-      >
-        <div
-          className={`${navAttributes} ${
-            type === "project" && "xl:max-w-screen-xl"
-          }`}
+    <Component className={navStyle}>
+      <div className={navContentStyle}>
+        <Link
+          href={sectionHomeRoute}
+          className="-ml-28 text-[22px] text-[#293a6b] lg:text-2xl"
         >
-          <Link
-            href={sectionHomeRoute}
-            className="-ml-8 text-[22px] text-[#293a6b] lg:text-2xl"
-          >
-            <RiArrowLeftSLine className="inline mb-4 -mr-[2px]" />
-            {title}
-          </Link>
-          <div className="flex gap-16 font-semibold text-base mr-16 sm:mr-0 lg:text-lg">
-            {prev && (
-              <Link href={`${path}/${prev}`} className="text-link">
-                previous
-                <span className="hidden md:inline">&nbsp;{type}</span>
-              </Link>
-            )}
-            {prev && next && <span className="font-normal">|</span>}
-            {next && (
-              <Link href={`${path}/${next}`} className="text-link">
-                next<span className="hidden md:inline">&nbsp;{type}</span>
-              </Link>
-            )}
-          </div>
+          <RiArrowLeftSLine className="inline mb-4 -mr-[2px]" aria-hidden />
+          {title}
+        </Link>
+        <div className="flex gap-16 font-semibold text-base mr-16 sm:mr-0 lg:text-lg">
+          {prev && (
+            <Link href={`${path}/${prev}`} className="text-link">
+              previous
+              <span className="hidden md:inline">&nbsp;{type}</span>
+            </Link>
+          )}
+          {prev && next && <span className="font-normal">|</span>}
+          {next && (
+            <Link href={`${path}/${next}`} className="text-link">
+              next<span className="hidden md:inline">&nbsp;{type}</span>
+            </Link>
+          )}
         </div>
       </div>
     </Component>
