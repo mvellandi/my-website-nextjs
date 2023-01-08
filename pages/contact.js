67600ffa6ed58d7cfe21419b610cd9f9cd719164
Mario Vellandi <mvellandi@hotmail.com>
2023-01-08 22:21:16 +0100

    + Add 'page' prop and value to PageLayout

--- pages/contact.js
+++ pages/contact.js
@@ -2,7 +2,7 @@ import PageLayout from "/components/page/Layout";
 
 export default function Contact({ preview = false }) {
   return (
-    <PageLayout>
+    <PageLayout page="contact">
       <h1 className="text-3xl text-black font-light -tracking-1 leading-tight">
         Contact
       </h1>
