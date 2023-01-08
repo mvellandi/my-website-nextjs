cdefa714b8d50fec6eb853dd1077bab56b4ae39d
Mario Vellandi <mvellandi@hotmail.com>
2023-01-08 22:08:36 +0100

    + Add 'page' prop to PageLayout this is for letting the SiteHeader know the current page, so it can be hidden in the navigation

--- components/page/Layout.js
+++ components/page/Layout.js
@@ -4,10 +4,10 @@ import ContentColumn from "/components/elements/ContentColumn";
 import Footer from "/components/site/Footer";
 
 // PAGES ARE FOR SINGLE-RECORD RESOURCES (e.g. /about) AND FOR STATIC PAGES (e.g. /contact)
-export default function PageLayout({ children }) {
+export default function PageLayout({ page, children }) {
   return (
     <>
-      <SiteHeader type="page" />
+      <SiteHeader type="page" page={page} />
       <HeaderOffset type="page" />
       <ContentColumn>{children}</ContentColumn>
       <Footer type="page" />
