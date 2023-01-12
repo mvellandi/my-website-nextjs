1e68162683cffecd8acf3b9f3f2aa94e3a21f233
Mario Vellandi <mvellandi@hotmail.com>
2023-01-12 11:54:07 +0100

    + css 'relative' position to body el for absolute overlays

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,10 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head />
-        <body>
+        <body className="relative">
           <Main />
           <NextScript />
         </body>
