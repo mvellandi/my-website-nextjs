58877b47f31965c253c13a19f39d37b7a8c4ea4b
Mario Vellandi <mvellandi@hotmail.com>
2023-02-17 16:32:55 +0100

    + Add html id="project"

--- components/project/ContentColumn.js
+++ components/project/ContentColumn.js
@@ -1,9 +1,12 @@
-export default function ContentColumn({ as, type, children }) {
+export default function ContentColumn({ as, children }) {
   const Component = as || "div";
 
   return (
     <div className="flex flex-col items-center bg-white site-padding-x">
-      <Component className="flex flex-col gap-40 md:gap-56 bg-white w-full max-w-[870px] pt-[50px] lg:pt-[80px] pb-[60px] lg:pb-[100px] xl:max-w-screen-xl xl:flex-none">
+      <Component
+        id="project"
+        className="flex flex-col gap-28 md:gap-40 bg-white w-full max-w-[870px] pt-[50px] lg:pt-[80px] pb-[60px] lg:pb-[100px] xl:max-w-screen-xl xl:flex-none xl:gap-52"
+      >
         {children}
       </Component>
     </div>
