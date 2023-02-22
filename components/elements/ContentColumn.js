44254e9fcd23838614a8e95f88ed25e7ef4fc1f3
Mario Vellandi <mvellandi@hotmail.com>
2023-02-22 13:34:40 +0100

    + Add 'id' prop for rendered root component * This will enable custom css/js targeting based on page type

--- components/elements/ContentColumn.js
+++ components/elements/ContentColumn.js
@@ -1,8 +1,8 @@
-export default function ContentColumn({ as, type, children }) {
+export default function ContentColumn({ id, as, type, children }) {
   const Component = as || "div";
 
   return (
-    <div className="flex flex-col items-center bg-white site-padding-x">
+    <div id={id} className="flex flex-col items-center bg-white site-padding-x">
       <Component className="bg-white w-full max-w-[870px] pt-[50px] lg:pt-[80px] pb-[60px] lg:pb-[100px]">
         {children}
       </Component>
