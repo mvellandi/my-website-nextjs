1ffb00beb79d6c4dec9dc3e54622be29a3472e01
Mario Vellandi <mvellandi@hotmail.com>
2023-05-06 01:18:26 +0200

    ~ mod meta head info + favicon and app icons

--- components/site/Meta.js
+++ components/site/Meta.js
@@ -3,40 +3,41 @@ import Head from "next/head";
 export default function Meta({ data }) {
   return (
     <Head>
-      {/* <link
+      <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/favicon/apple-touch-icon.png"
+        href="/meta/apple-touch-icon.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="/favicon/favicon-32x32.png"
+        href="/meta/favicon-32x32.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="/favicon/favicon-16x16.png"
+        href="/meta/favicon-16x16.png"
       />
-      <link rel="manifest" href="/favicon/site.webmanifest" />
-      <link
+      <link rel="manifest" href="/meta/site.webmanifest" />
+      {/* this sets the color of url bar */}
+      <meta name="theme-color" content="#c93b53" />
+      {/* sets the color of url bar in Apple phones */}
+      <meta name="apple-mobile-web-app-status-bar" content="#90cdf4" />
+      {/* <link
         rel="mask-icon"
-        href="/favicon/safari-pinned-tab.svg"
+        href="/meta/safari-pinned-tab.svg"
         color="#000000"
       />
       <meta name="msapplication-TileColor" content="#000000" />
-      <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
+      <meta name="msapplication-config" content="/meta/browserconfig.xml" />
       <meta name="theme-color" content="#000" />
-    <link rel="alternate" type="application/rss+xml" href="/feed.xml" /> */}
-      {/* <meta property="og:image" content={HOME_OG_IMAGE_URL} /> */}
-      <title>Sample Title</title>
-      <meta
-        name="description"
-        content={`A statically generated site example using Next.js and Sanity.io.`}
-      />
-      {/* <link rel="shortcut icon" href="/favicon/favicon.ico" /> */}
+    <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+      <meta property="og:image" content={HOME_OG_IMAGE_URL} /> */}
+      <title>{data.title}</title>
+      <meta name="description" content={data.description} />
+      <link rel="shortcut icon" href="/meta/favicon.ico" />
     </Head>
   );
 }
