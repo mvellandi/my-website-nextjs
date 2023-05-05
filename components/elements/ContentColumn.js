a37956b1934184ff4388a682065c06c08794e1dd
Mario Vellandi <mvellandi@hotmail.com>
2023-05-05 02:12:32 +0200

    + min component height

--- components/elements/ContentColumn.js
+++ components/elements/ContentColumn.js
@@ -3,7 +3,7 @@ export default function ContentColumn({ id, as, type, children }) {
 
   return (
     <div id={id} className="flex flex-col items-center bg-white site-padding-x">
-      <Component className="bg-white w-full max-w-[870px] pt-[50px] lg:pt-[80px] pb-[60px] lg:pb-[100px]">
+      <Component className="bg-white w-full max-w-[870px] min-h-[450px] pt-[50px] lg:pt-[80px] pb-[60px] lg:pb-[100px] 2k3:min-h-[550px]">
         {children}
       </Component>
     </div>
