18a8d4f3475243afd5d77ddd31ca8850dd997fef
Mario Vellandi <mvellandi@hotmail.com>
2023-12-14 08:25:37 +0100

    + add notes about css nesting plugin

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,7 @@
+// The 'nesting' plugin by tailwind adapts the 'postcss-nesting' plugin and CSS Nesting Module Draft specification:
+// https://github.com/csstools/postcss-plugins/tree/main/plugins/postcss-nesting
+// https://www.w3.org/TR/css-nesting-1/
+
 module.exports = {
   plugins: {
     "postcss-import": {},
