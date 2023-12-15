3e300970e325d1117492817817730a34fb42ae26
Mario Vellandi <mvellandi@hotmail.com>
2023-12-15 17:04:13 +0100

    ~ adjust minor syntax, visual spacing

--- components/elements/ContentColumn.js
+++ components/elements/ContentColumn.js
@@ -1,9 +1,9 @@
 export default function ContentColumn({ id, as, type, children }) {
-  const Component = as || "div";
+  const Component = as ?? "div";
 
   return (
     <div id={id} className="flex flex-col items-center bg-white site-padding-x">
-      <Component className="bg-white w-full max-w-[870px] min-h-[450px] pt-[50px] lg:pt-[80px] pb-[60px] lg:pb-[100px] 2k3:min-h-[550px]">
+      <Component className="bg-white w-full max-w-[870px] min-h-[450px] pt-[40px] pb-[60px] lg:pt-[70px] lg:pb-[100px] 2k3:min-h-[550px]">
         {children}
       </Component>
     </div>
--- components/project/ContentColumn.js
+++ components/project/ContentColumn.js
@@ -1,11 +1,11 @@
 export default function ContentColumn({ as, children }) {
-  const Component = as || "div";
+  const Component = as ?? "div";
 
   return (
     <div className="flex flex-col items-center bg-white site-padding-x">
       <Component
         id="project"
-        className="flex flex-col gap-28 md:gap-40 bg-white w-full max-w-[870px] pt-[50px] lg:pt-[80px] pb-[60px] lg:pb-[100px] xl:max-w-screen-xl xl:flex-none xl:gap-52"
+        className="flex flex-col gap-24 md:gap-28 bg-white w-full max-w-[870px] pt-[50px] lg:pt-[70px] pb-[60px] lg:pb-[100px] xl:max-w-screen-xl xl:flex-none xl:gap-40"
       >
         {children}
       </Component>
