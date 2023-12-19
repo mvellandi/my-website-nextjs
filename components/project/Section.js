31042fabcd964750021393a736a447124fdd35e0
Mario Vellandi <mvellandi@hotmail.com>
2023-12-19 21:24:17 +0100

    ~ dramatically simplify component to be a wrapper for children elements

--- components/project/Section.js
+++ components/project/Section.js
@@ -1,102 +1,26 @@
 // --------------------------------------------------------------
-// This component is used to create a section in the project page.
-// This could be a one of the following:
-// * A independent section
-// * An outline
-// * An outline section
+// Project sections are wrappers for:
+// * Static or dynamic text blocks with unknown content
+// * A custom section (links, structure, features, etc.)
 // --------------------------------------------------------------
 
-import { sectionGap } from "./Item";
+import cn from "classnames";
 
-function idAnchor(string) {
-  return string
-    .replace(/[^\w\s]/gi, "")
-    .replace(/\s+/g, "-")
-    .toLowerCase();
-}
+export const sectionHeadingStyle =
+  "mb-4 text-xl font-bold text-orange lg:text-xl lg:mb-6";
 
-export default function Section({
-  key,
-  isOutline,
-  isOutlineSection,
-  title,
-  children,
-}) {
-  // If the section is an outline, then it will have a different style
-  isOutline ??= false;
-  isOutlineSection ??= false;
-  // Root component type
-  const Component = isOutline ? "nav" : "section";
-  // Get the name of the child component
-  const childType = children.type.name || children.type;
-  // styles
-  const headingStyle = `mb-4 text-xl font-bold text-orange lg:text-xl lg:mb-6 ${
-    isOutline || isOutlineSection
-      ? "scroll-mt-[15rem] sm:scroll-mt-[16.5rem] md:scroll-mt-[23rem] lg:scroll-mt-[26rem]"
-      : ""
-  }`;
+export default function Section({ title, className, children }) {
+  className ??= "standard";
+  const componentProps = {
+    className: cn("section", className),
+  };
 
   return (
-    <Component key={key}>
-      {/* ---------------- */}
+    <section {...componentProps}>
       {/* HEADER */}
-      {/* ---------------- */}
-      {/* --- INDEPENDENT SECTION OR OUTLINE */}
-      {(!isOutlineSection || isOutline) && (
-        <h2 className={headingStyle}>{title}</h2>
-      )}
-      {/* --- OUTLINE SECTION */}
-      {isOutlineSection && (
-        <div className={`flex items-start justify-between`}>
-          <h2
-            id={idAnchor(title)}
-            className={`${headingStyle} scroll-mt-[15rem] sm:scroll-mt-[16.5rem] md:scroll-mt-[23rem] lg:scroll-mt-[26rem]`}
-          >
-            {title}
-          </h2>
-          <div
-            className="block pl-20 text-sm text-gray-400 min-w-[120px] xl:hidden"
-            aria-hidden
-          >
-            <a href="#" className="text-link">
-              top
-            </a>{" "}
-            |{" "}
-            <a href="#outline" className="text-link">
-              outline
-            </a>
-          </div>
-        </div>
-      )}
-      {/* ---------------- */}
+      <h2 className={sectionHeadingStyle}>{title}</h2>
       {/* BODY */}
-      {/* ---------------- */}
-      {/* --- INDEPENDENT SECTION */}
-      {(() => {
-        if (!isOutline && !isOutlineSection) {
-          if (childType === "Media") {
-            return children;
-          } else {
-            return <div className="body">{children}</div>;
-          }
-        }
-      })()}
-      {/* --- OUTLINE OR OUTLINE SECTION */}
-      {(isOutline || isOutlineSection) && (
-        <div
-          className={`
-          ${sectionGap} 
-          ${isOutlineSection && "body"}
-          mt-${isOutline ? "4" : "3"} 
-          ${
-            isOutline &&
-            "[&>ol]:space-y-6 [&>ol>li>a]:text-lg [&>ol>li>a]:no-underline"
-          }
-        `}
-        >
-          {children}
-        </div>
-      )}
-    </Component>
+      {children}
+    </section>
   );
 }
