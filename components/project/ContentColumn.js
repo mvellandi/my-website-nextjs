1456b770f4c196b6fb0b861a311c1df4acc8b796
Mario Vellandi <mvellandi@hotmail.com>
2023-12-19 21:09:54 +0100

    ~ add html comments, project class, mod gap vals

--- components/project/ContentColumn.js
+++ components/project/ContentColumn.js
@@ -1,14 +1,17 @@
+import HTMLComment from "react-html-comment";
+
 export default function ContentColumn({ as, children }) {
   const Component = as ?? "div";
 
   return (
-    <div className="flex flex-col items-center bg-white site-padding-x">
-      <Component
-        id="project"
-        className="flex flex-col gap-24 md:gap-28 bg-white w-full max-w-[870px] pt-[50px] lg:pt-[70px] pb-[60px] lg:pb-[100px] xl:max-w-screen-xl xl:flex-none xl:gap-40"
-      >
-        {children}
-      </Component>
-    </div>
+    <>
+      <HTMLComment text="CENTER COLUMN" />
+      <div className="flex flex-col items-center bg-white site-padding-x">
+        <HTMLComment text="CONTENT ITEM" />
+        <Component className="project flex flex-col gap-14 sm:gap-24 bg-white w-full max-w-[870px] pt-[50px] lg:pt-[70px] pb-[60px] lg:pb-[100px] xl:max-w-screen-xl xl:flex-none xl:gap-40">
+          {children}
+        </Component>
+      </div>
+    </>
   );
 }
