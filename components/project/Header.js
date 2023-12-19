db14f69d31fceccd6de43a5ac742a5567c46f87a
Mario Vellandi <mvellandi@hotmail.com>
2023-12-19 21:12:44 +0100

    ~ add code/display helpers; mod name size, minor syntax

--- components/project/Header.js
+++ components/project/Header.js
@@ -1,15 +1,26 @@
+import cn from "classnames";
+
 export default function Header({ name, sector, coverImage }) {
+  function smallerText(text) {
+    return text.length > 30 ? "text-2xl" : "text-3xl";
+  }
+
   return (
     <header className="flex justify-between w-full gap-24 sm:justify-start sm:gap-36">
       <div className="flex flex-col gap-8 sm:order-2">
-        <h1 className="text-3xl font-light leading-tight text-black -tracking-1 sm:text-3xl">
+        <h1
+          className={cn(
+            smallerText(name),
+            "font-light leading-[1.3] text-black -tracking-1 sm:text-3xl"
+          )}
+        >
           {name}
         </h1>
         <p className="text-sm font-bold text-gray-700 uppercase tracking-3">
           {sector}
         </p>
       </div>
-      <div class="pt-6 min-w-[100px] max-w-[120px] lg:pt-8">
+      <div className="pt-6 min-w-[100px] max-w-[120px] lg:pt-8">
         <img
           src={coverImage}
           alt="altText"
