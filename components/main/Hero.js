e48390fcffdebfe6fb51e620e17ad02a7fc99889
Mario Vellandi <mvellandi@hotmail.com>
2023-12-20 15:06:48 +0100

    ~ add/remove classes and comments

--- components/main/Hero.js
+++ components/main/Hero.js
@@ -13,18 +13,15 @@ export default function Hero({ as }) {
   const Component = as ?? "div";
   return (
     <>
-      <HTMLComment text="HERO SECTION" />
       {/* SECTION BACKGROUND + CONTENT ROW: full-width, h-centered children */}
-      <Component className="flex justify-center w-full gap-16 pt-16 pb-24 text-white border-b-8 site-padding-x sm:gap-48 md:pb-32 md:pt-8 lg:pt-0 lgtall:pt-20 lgtall:pb-40 2k:pb-48 2k:pt-16 bg-red border-red-shade">
+      <Component className="flex justify-center w-full gap-16 pt-16 pb-24 text-white border-b-8 hero site-padding-x sm:gap-48 md:pb-32 md:pt-8 lg:pt-0 lgtall:pt-20 lgtall:pb-40 2k:pb-48 2k:pt-16 bg-red border-red-shade">
         {/*  */}
         <HTMLComment text="HERO TEXT" />
         <div className="flex flex-col justify-center gap-24 md:w-[380px] lg:w-[440px] lg:gap-48">
           <h1 className="relative order-2 w-fit">
-            <Link className="target main-services" href="/services">
+            <Link className="target cta" href="/services">
               <span aria-hidden>{"\u2192"} </span>
-              <span className="main-services-text">
-                Let&rsquo;s work together
-              </span>
+              <span className="text">Let&rsquo;s work together</span>
             </Link>
           </h1>
           <h2 className="text-xl -tracking-3 font-light drop-shadow order-1 pmd:text-2xl md:text-3xl md:leading-[1.22] lgtall:text-[4.2rem]">
