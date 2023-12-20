5c8e1524bb20d9c997b00abb137882fe05d4f7c4
Mario Vellandi <mvellandi@hotmail.com>
2023-12-20 15:08:16 +0100

    ~ add/remove classes, comments, wrap layout in div with main class

--- components/main/Layout.js
+++ components/main/Layout.js
@@ -4,23 +4,29 @@ import FixedHeaderOffset from "/components/elements/FixedHeaderOffset";
 import Hero from "/components/main/Hero";
 import Nav from "/components/main/Nav";
 import Footer from "/components/site/Footer";
+import HTMLComment from "react-html-comment";
 
 // TODO: Eventually make this only a Homepage layout
 
 export default function MainLayout({ data, children, preview }) {
   return (
     <>
+      <HTMLComment text="MAIN LAYOUT" />
       {/* SITE HEADER FOR MOBILE AND SCREEN READERS HERE; INCLUDING ALL NAV LINKS IN ONE PLACE */}
       {/* To avoid having adjacent navigation, consider moving SiteHeader above footer in code */}
       {/* then rearranging it visually using flexbox order */}
-      <FixedHeader>
-        <SiteHeader type="main" />
-      </FixedHeader>
-      <FixedHeaderOffset type="main" />
-      <Hero as="section" />
-      <Nav active={data.name} />
-      {children}
-      <Footer type="main" />
+      <div className="main">
+        <FixedHeader>
+          <SiteHeader type="main" />
+        </FixedHeader>
+        <FixedHeaderOffset type="main" />
+        <HTMLComment text="HERO SECTION" />
+        <Hero as="section" />
+        <HTMLComment text="CONTENT TYPE NAVIGATION" />
+        <Nav active={data.name} />
+        {children}
+        <Footer type="main" />
+      </div>
     </>
   );
 }
