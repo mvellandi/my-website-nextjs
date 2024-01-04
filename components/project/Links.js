187a13a58d1001b472557e9a5965d32878197d7b
Mario Vellandi <mvellandi@hotmail.com>
2024-01-04 15:48:00 +0100

    ~ adjust font size smaller on large screens

--- components/project/Links.js
+++ components/project/Links.js
@@ -7,7 +7,7 @@ export default function Links({ links }) {
   return (
     <Section title="Project Links" className="links">
       <ul
-        className={`flex flex-col list-none gap-4 pl-0 pt-4 text-[1.9rem] tracking-tight leading-[1.6] md:text-[2rem] md:gap-6 lg:text-[2.2rem] lg:gap-8 xl:gap-6`}
+        className={`flex flex-col list-none gap-4 pl-0 pt-4 text-[1.9rem] tracking-tight leading-[1.6] md:text-[2rem] md:gap-8`}
       >
         {links.map(({ _key, text, url }) => (
           <li key={_key} className="font-semibold">
