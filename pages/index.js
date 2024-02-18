589577cba37a30a081cc4af76fd4224b2bfa5d72
Mario Vellandi <mvellandi@hotmail.com>
2024-02-18 16:14:14 +0100

    ~ update meta title

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,34 @@
-import Meta from "/components/site/Meta";
-import MainLayout from "/components/main/Layout";
-import Items from "/components/main/Items";
-import { getAllProjectCards } from "/lib/project";
+import Meta from '/components/site/Meta'
+import MainLayout from '/components/main/Layout'
+import Items from '/components/main/Items'
+import { getAllProjectCards } from '/lib/project'
 
 // TODO: Change the layout to list all section items, in 3 separate lists
 
 export default function Home({ data, preview }) {
-  let meta = {
-    title: "Mario Vellandi: Digital Product Development and Content Strategy",
-    description:
-      "Portfolio, services offered, articles, and quick projects by Mario Vellandi.",
-    // image: "/images/og-image.png",
-  };
-  return (
-    <>
-      <Meta data={meta} />
-      <MainLayout preview={preview} data={data}>
-        <Items as="main" data={data} />
-      </MainLayout>
-    </>
-  );
+    let meta = {
+        title: 'Mario Vellandi: Software Engineer and Product Developer for Elixir, JS, and CSS',
+        description:
+            'Portfolio, services offered, articles, and quick projects by Mario Vellandi.',
+        // image: "/images/og-image.png",
+    }
+    return (
+        <>
+            <Meta data={meta} />
+            <MainLayout preview={preview} data={data}>
+                <Items as="main" data={data} />
+            </MainLayout>
+        </>
+    )
 }
 
 export async function getStaticProps({ preview = false }) {
-  const data = await getAllProjectCards({ preview });
-  return {
-    props: {
-      data,
-      preview,
-    },
-    revalidate: 1,
-  };
+    const data = await getAllProjectCards({ preview })
+    return {
+        props: {
+            data,
+            preview,
+        },
+        revalidate: 1,
+    }
 }
