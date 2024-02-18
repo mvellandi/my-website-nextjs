deb9a8b4cc99239c99b6df4e4136cc86fd69151c
Mario Vellandi <mvellandi@hotmail.com>
2024-02-18 16:17:53 +0100

    ~ add process section nav on large screens

--- components/project/ProcessSection.js
+++ components/project/ProcessSection.js
@@ -1,40 +1,40 @@
-import { PortableText } from "@portabletext/react";
+import { PortableText } from '@portabletext/react'
 
 function idAnchor(string) {
-  return string
-    .replace(/[^\w\s]/gi, "")
-    .replace(/\s+/g, "-")
-    .toLowerCase();
+    return string
+        .replace(/[^\w\s]/gi, '')
+        .replace(/\s+/g, '-')
+        .toLowerCase()
 }
 
 export default function ProcessSection({ section, iteration }) {
-  return (
-    <section className="section standard">
-      {/* HEADING WITH QUICK NAVIGATION FOR SMALLER SCREENS */}
-      <div className={`flex items-start justify-between`}>
-        <h2
-          id={`${idAnchor(section.heading)}-${iteration}`}
-          className="heading scroll_margin"
-        >
-          {section.heading}
-        </h2>
-        <div
-          className="flex flex-col justify-end mt-auto xl:hidden"
-          aria-hidden="true"
-        >
-          <div className="pl-20 pb-6 text-sm text-gray-400 min-w-[120px]">
-            <a href="#" className="text-link">
-              top
-            </a>{" "}
-            |{" "}
-            <a href={`#outline-${iteration}`} className="text-link">
-              outline
-            </a>
-          </div>
-        </div>
-      </div>
-      {/* BODY */}
-      <PortableText value={section.body} />
-    </section>
-  );
+    return (
+        <section className="section standard">
+            {/* HEADING WITH QUICK NAVIGATION */}
+            <div className={`flex items-start justify-between`}>
+                <h2
+                    id={`${idAnchor(section.heading)}-${iteration}`}
+                    className="heading scroll_margin"
+                >
+                    {section.heading}
+                </h2>
+                <div
+                    className="mt-auto flex flex-col justify-end"
+                    aria-hidden="true"
+                >
+                    <div className="min-w-[120px] pb-6 pl-20 text-sm text-gray-400">
+                        <a href="#" className="text-link">
+                            top
+                        </a>{' '}
+                        |{' '}
+                        <a href={`#outline-${iteration}`} className="text-link">
+                            outline
+                        </a>
+                    </div>
+                </div>
+            </div>
+            {/* BODY */}
+            <PortableText value={section.body} />
+        </section>
+    )
 }
