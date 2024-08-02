c9dfb613544e7b48d4660085e5fb293f98aac8e7
Mario Vellandi <mvellandi@hotmail.com>
2024-08-02 22:20:37 +0200

    ~ refactor

--- components/article/Item.js
+++ components/article/Item.js
@@ -1,22 +1,47 @@
 /* eslint-disable @next/next/no-img-element */
-import { urlForImage } from "/lib/sanity";
-import Link from "next/link";
-import { PortableText } from "@portabletext/react";
+import { urlForImage } from '/lib/sanity'
+import Link from 'next/link'
+import urlBuilder from '@sanity/image-url'
+import { imageBuilder } from '../../lib/sanity'
+import { getImageDimensions } from '@sanity/asset-utils'
+import { PortableText } from '@portabletext/react'
+
+const ImageComponent = ({ node }) => {
+    const { width, height } = getImageDimensions(node)
+    return (
+        <img
+            src={imageBuilder.image(node).url()}
+            alt={node.alt || ' '}
+            loading="lazy"
+            style={{ aspectRatio: width / height }}
+        />
+    )
+}
 
 export default function Item({ data }) {
-  const { headline, body } = data;
+    const { headline, body } = data
 
-  return (
-    <>
-      <h1 className="mb-20 text-3xl font-light leading-tight text-black text-balance -tracking-1 lg:mb-40">
-        {headline}
-      </h1>
-      <div className="prose-lg lg:prose-xl">
-        {(() => {
-          const [_type, content] = [body._type, body.body];
-          return <PortableText value={content} onMissingComponent={false} />;
-        })()}
-      </div>
-    </>
-  );
+    return (
+        <>
+            <h1 className="mb-20 text-balance text-3xl font-light leading-tight -tracking-1 text-black lg:mb-40">
+                {headline}
+            </h1>
+            <div className="prose-lg lg:prose-xl">
+                {(() => {
+                    const [_type, content] = [body._type, body.body]
+                    return (
+                        <PortableText
+                            value={content}
+                            onMissingComponent={false}
+                            components={{
+                                types: {
+                                    image: ImageComponent,
+                                },
+                            }}
+                        />
+                    )
+                })()}
+            </div>
+        </>
+    )
 }
