57f7cebcc1ed72afabace52c06ec1ea983931aad
Mario Vellandi <mvellandi@hotmail.com>
2024-09-27 22:14:23 +0200

    ~ reformat

--- pages/articles/[slug].js
+++ pages/articles/[slug].js
@@ -1,40 +1,40 @@
-import { getArticleAndNavData, getAllArticlesWithSlug } from "/lib/article";
-import SectionLayout from "/components/section/Layout";
-import ArticleItem from "/components/article/Item";
+import { getArticleAndNavData, getAllArticlesWithSlug } from '/lib/article'
+import SectionLayout from '/components/section/Layout'
+import ArticleItem from '/components/article/Item'
 
 export default function Article({ nav, article, preview = false }) {
-  return (
-    <SectionLayout type="article" nav={nav}>
-      <ArticleItem data={article} />
-    </SectionLayout>
-  );
+    return (
+        <SectionLayout type="article" nav={nav}>
+            <ArticleItem data={article} />
+        </SectionLayout>
+    )
 }
 
 export async function getStaticProps({ params, preview = false }) {
-  const data = await getArticleAndNavData({
-    slug: params.slug,
-    preview,
-  });
-  return {
-    props: {
-      nav: data.nav,
-      article: data.article,
-      preview,
-    },
-    revalidate: 1,
-  };
+    const data = await getArticleAndNavData({
+        slug: params.slug,
+        preview,
+    })
+    return {
+        props: {
+            nav: data.nav,
+            article: data.article,
+            preview,
+        },
+        revalidate: 1,
+    }
 }
 
 // For each project, compute the previous/next project slugs and pass them to each
 export async function getStaticPaths() {
-  const allArticles = await getAllArticlesWithSlug();
-  return {
-    paths:
-      allArticles?.map((article) => ({
-        params: {
-          slug: article.slug,
-        },
-      })) || [],
-    fallback: false,
-  };
+    const allArticles = await getAllArticlesWithSlug()
+    return {
+        paths:
+            allArticles?.map((article) => ({
+                params: {
+                    slug: article.slug,
+                },
+            })) || [],
+        fallback: false,
+    }
 }
