b87bd1f8853f39e49b841b1d5a33f108f22b031d
Mario Vellandi <mvellandi@hotmail.com>
2024-09-30 22:16:30 +0200

    ~ add comments, ability to link to local pages

--- components/main/Card.js
+++ components/main/Card.js
@@ -1,39 +1,45 @@
-import Image from "next/image";
-import { urlForImage } from "/lib/sanity";
-import Link from "next/link";
+import Image from 'next/image'
+import { urlForImage } from '/lib/sanity'
+import Link from 'next/link'
 
 export default function Card({
-  data: { _id, title, subtitle, slug, url, coverImage },
-  route,
-  as,
+    data: { _id, title, subtitle, slug, url, coverImage, local },
+    route,
+    as,
 }) {
-  const Component = as ?? "div";
-  return (
-    <Component className="card">
-      {/* Reordered to semantically prioritize titles, though image will visually appear first */}
-      <div className="flex flex-col gap-[2px] order-2 -mt-2">
-        <h2 className="card-link text-balance text-gray-900 font-bold -tracking-1 leading-[1.22] text-base lg:text-lg lg:leading-[1.2] 2k:leading-[1.4]">
-          {url ? (
-            <Link href={url} target="_blank" rel="noopener noreferrer">
-              {title}
-            </Link>
-          ) : (
-            <Link href={`${route}/${slug}`}>{title}</Link>
-          )}
-        </h2>
-        {subtitle ? (
-          <h3 className="text-sm leading-[1.375] sm:text-[1.7rem] sm:leading-[1.337] lg:text-base lg:leading-[1.3] 3xl:leading-[1.4]">
-            {subtitle}
-          </h3>
-        ) : null}
-      </div>
-      <Image
-        src={urlForImage(coverImage).url()}
-        alt="altText"
-        width={100}
-        height={100}
-        className="w-[90px] h-[90px] lg:w-[100px] lg:h-[100px] order-1"
-      />
-    </Component>
-  );
+    const Component = as ?? 'div'
+    return (
+        <Component className="card">
+            {/* Though the card image will visually appear first, the code prioritizes the h2 title */}
+            <div className="order-2 -mt-2 flex flex-col gap-[2px]">
+                <h2 className="card-link text-balance text-base font-bold leading-[1.22] -tracking-1 text-gray-900 lg:text-lg lg:leading-[1.2] 2k:leading-[1.4]">
+                    {/* Cards might point to an external URL (if available) */}
+                    {/* OTHERWISE to an internal static page or an asset-page (if URL is not available) */}
+                    {url ? (
+                        <Link
+                            href={url}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            {title}
+                        </Link>
+                    ) : (
+                        <Link href={`${route}/${slug}`}>{title}</Link>
+                    )}
+                </h2>
+                {subtitle ? (
+                    <h3 className="text-sm leading-[1.375] sm:text-[1.7rem] sm:leading-[1.337] lg:text-base lg:leading-[1.3] 3xl:leading-[1.4]">
+                        {subtitle}
+                    </h3>
+                ) : null}
+            </div>
+            <Image
+                src={local ? coverImage : urlForImage(coverImage).url()}
+                alt="altText"
+                width={100}
+                height={100}
+                className="order-1 h-[90px] w-[90px] lg:h-[100px] lg:w-[100px]"
+            />
+        </Component>
+    )
 }
