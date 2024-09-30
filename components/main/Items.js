21438f178d4c6205b7c677542d610695d3a0bac0
Mario Vellandi <mvellandi@hotmail.com>
2024-09-30 22:17:04 +0200

    ~ minor formatting update

--- components/main/Items.js
+++ components/main/Items.js
@@ -1,18 +1,25 @@
-import Card from "/components/main/Card";
-import HTMLComment from "react-html-comment";
+import Card from '/components/main/Card'
+import HTMLComment from 'react-html-comment'
 
-export default function Items({ data: { type, route, value: items }, as }) {
-  const Component = as ?? "div";
-  return (
-    <>
-      <HTMLComment text="CONTENT ITEMS (DEFAULT: PROJECTS)" />
-      <Component className="bg-white flex justify-center items-start w-full site-padding-x pt-20 pb-36 min-h-[180px] sm:min-h-[300px] md:py-36 md:min-h-[380px] lgtall:pt-48 lgtall:pb-56 lgtall:min-h-[450px] xl:min-h-[250px] 2xl:min-h-[350px] 3xl:min-h-[400px] 2k:pt-56 2k:pb-64 2k3:min-h-[480px]">
-        <ul className="w-full max-w-screen-xl grid justify-center grid-cols-1 gap-24 md:grid-cols-2 md:gap-48 md:min-w-[620px] xl:grid-cols-3 2xl:gap-36 2k:gap-64">
-          {items.map((item) => {
-            return <Card as="li" key={item._id} route={route} data={item} />;
-          })}
-        </ul>
-      </Component>
-    </>
-  );
+export default function Items({ data: { _type, route, value: items }, as }) {
+    const Component = as ?? 'div'
+    return (
+        <>
+            <HTMLComment text="CONTENT ITEMS (DEFAULT: PROJECTS)" />
+            <Component className="site-padding-x flex min-h-[180px] w-full items-start justify-center bg-white pb-36 pt-20 sm:min-h-[300px] md:min-h-[380px] md:py-36 lgtall:min-h-[450px] lgtall:pb-56 lgtall:pt-48 xl:min-h-[250px] 2xl:min-h-[350px] 3xl:min-h-[400px] 2k:pb-64 2k:pt-56 2k3:min-h-[480px]">
+                <ul className="grid w-full max-w-screen-xl grid-cols-1 justify-center gap-24 md:min-w-[620px] md:grid-cols-2 md:gap-48 xl:grid-cols-3 2xl:gap-36 2k:gap-64">
+                    {items.map((item) => {
+                        return (
+                            <Card
+                                as="li"
+                                key={item._id}
+                                route={route}
+                                data={item}
+                            />
+                        )
+                    })}
+                </ul>
+            </Component>
+        </>
+    )
 }
