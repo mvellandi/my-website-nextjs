c9ea867dd9a861e600b609b7aea689bbe5e35b04
Mario Vellandi <mvellandi@hotmail.com>
2024-10-01 14:40:37 +0200

    ~ move getting slugs to lib module ~ rename funcs and variables to reflect getting 'slugs'

--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -1,40 +1,35 @@
-import { getProjectAndNavData, getAllProjectsWithSlug } from "/lib/project";
-import SectionLayout from "/components/section/Layout";
-import ProjectItem from "/components/project/Item";
+import { getProjectAndNavData, getAllProjectSlugs } from '/lib/project'
+import SectionLayout from '/components/section/Layout'
+import ProjectItem from '/components/project/Item'
 
 export default function Project({ nav, project, preview = false }) {
-  return (
-    <SectionLayout type="project" nav={nav}>
-      <ProjectItem data={project} />
-    </SectionLayout>
-  );
+    return (
+        <SectionLayout type="project" nav={nav}>
+            <ProjectItem data={project} />
+        </SectionLayout>
+    )
 }
 
 export async function getStaticProps({ params, preview = false }) {
-  const data = await getProjectAndNavData({
-    slug: params.slug,
-    preview,
-  });
-  return {
-    props: {
-      nav: data.nav,
-      project: data.project,
-      preview,
-    },
-    revalidate: 1,
-  };
+    const data = await getProjectAndNavData({
+        slug: params.slug,
+        preview,
+    })
+    return {
+        props: {
+            nav: data.nav,
+            project: data.project,
+            preview,
+        },
+        revalidate: 1,
+    }
 }
 
 // For each project, compute the previous/next project slugs and pass them to each
 export async function getStaticPaths() {
-  const allProjects = await getAllProjectsWithSlug();
-  return {
-    paths:
-      allProjects?.map((project) => ({
-        params: {
-          slug: project.slug,
-        },
-      })) || [],
-    fallback: false,
-  };
+    const slugs = await getAllProjectSlugs()
+    return {
+        paths: slugs || [],
+        fallback: false,
+    }
 }
