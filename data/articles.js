239138f641358dc59ee2b6663d22289c06a9b14a
Mario Vellandi <mvellandi@hotmail.com>
2024-10-01 12:10:14 +0200

    ~ move non-CMS card data to 'data' dir; rename 'local' to 'isLocalImage'

---
+++ data/articles.js
@@ -0,0 +1,27 @@
+// These are extra article cards to include in the articles index page
+// The cards refer to either static local article pages or external URLs
+
+// THE IDs needs to be unique and manually updated
+
+export const firstArticles = [
+    {
+        isLocalImage: true,
+        _id: '14528482166013190',
+        slug: 'writing-collection',
+        coverImage: '/media/writing-collection.jpg',
+        title: 'Written Works',
+        subtitle: 'Journalism, interviews, and copywriting, oh my!',
+    },
+]
+
+export const lastArticles = [
+    {
+        isLocalImage: true,
+        _id: '14528482166013191',
+        url: 'https://old-journal.vellandi.net',
+        coverImage: '/media/wordpress-logo.svg',
+        title: 'Blog Archive 2006-2016',
+        subtitle:
+            '618 posts on marketing, design, product development, and sustainability',
+    },
+]
--- pages/articles.js
+++ pages/articles.js
@@ -2,6 +2,7 @@ import Meta from '/components/site/Meta'
 import MainLayout from '/components/main/Layout'
 import Items from '/components/main/Items'
 import { getAllArticleCards } from '/lib/article'
+import { firstArticles, lastArticles } from '/data/articles'
 
 // TODO: Change the layout from MainLayout to a "articles index" layout
 
@@ -18,31 +19,7 @@ export default function Articles({ data, preview }) {
 
 export async function getStaticProps({ preview = false }) {
     const data = await getAllArticleCards({ preview })
-
-    const featured = [
-        {
-            local: true,
-            _id: '14528482166013190',
-            slug: 'writing-collection',
-            coverImage: '/media/writing-collection.jpg',
-            title: 'Written Works',
-            subtitle: 'Journalism, interviews, and copywriting, oh my!',
-        },
-    ]
-
-    const oldMedia = [
-        {
-            local: true,
-            _id: '14528482166013191',
-            url: 'https://old-journal.vellandi.net',
-            coverImage: '/media/wordpress-logo.svg',
-            title: 'Blog Archive 2006-2016',
-            subtitle:
-                'On marketing, design, product development, and sustainability',
-        },
-    ]
-
-    data.value = [...featured, ...data.value, ...oldMedia]
+    data.value = [...firstArticles, ...data.value, ...lastArticles]
 
     console.log('data: ', data)
     return {
