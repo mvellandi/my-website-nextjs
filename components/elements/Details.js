733e42644fba8ef33bdd2a8a9ffb2e8b0a749b56
Mario Vellandi <mvellandi@hotmail.com>
2024-10-03 08:57:23 +0200

    ~ mod heading font weights

--- components/elements/Details.js
+++ components/elements/Details.js
@@ -3,7 +3,7 @@ export default function Details({ summary, children }) {
         <details className="group mb-10 lg:mt-12">
             <summary className="cursor-pointer list-none">
                 <div className="flex w-full justify-between">
-                    <h2 className="relative m-0 w-full border border-[#dadada] bg-[#e7e7e7] py-2 pl-6 font-semibold lg:p-6 lg:text-lg lg:font-normal">
+                    <h2 className="relative m-0 w-full border border-[#dadada] bg-[#e7e7e7] py-2 pl-6 font-semibold lg:p-6 lg:text-lg lg:font-[500]">
                         <span className="absolute -left-20 top-1/2 -translate-y-1/2 text-sm transition-transform group-open:rotate-90 lg:-left-28">
                             &#9654;
                         </span>
