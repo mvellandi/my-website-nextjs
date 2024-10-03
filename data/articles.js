03105f8d49f8cacd8b3a2718fd454b724e93a5db
Mario Vellandi <mvellandi@hotmail.com>
2024-10-03 08:46:42 +0200

    ~ rename media dir to images

--- data/articles.js
+++ data/articles.js
@@ -8,7 +8,7 @@ export const firstArticles = [
         isCMSImage: false,
         _id: '14528482166013190',
         slug: 'writing-collection',
-        coverImage: '/media/writing-collection.jpg',
+        coverImage: '/images/writing-collection.jpg',
         title: 'Written Works',
         subtitle: 'Journalism, interviews, and copywriting, oh my!',
     },
@@ -19,7 +19,7 @@ export const lastArticles = [
         isCMSImage: false,
         _id: '14528482166013191',
         url: 'https://old-journal.vellandi.net',
-        coverImage: '/media/wordpress-logo.svg',
+        coverImage: '/images/wordpress-logo.svg',
         title: 'Blog Archive 2006-2016',
         subtitle:
             '618 posts on marketing, design, product development, and sustainability',
