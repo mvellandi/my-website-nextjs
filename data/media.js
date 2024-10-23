b55f733981ce65d0b2cfe4d9e91dc46add737a14
Mario Vellandi <mvellandi@hotmail.com>
2024-10-23 12:24:46 +0200

    create list of all infographics with title and src

---
+++ data/media.js
@@ -0,0 +1,50 @@
+export const infographics = [
+    {
+        title: 'Word of Mouth Marketing Visualized',
+        src: '/images/blog-wom.jpg',
+    },
+    {
+        title: 'Himmel User Flow',
+        src: '/images/himmel-user-flow.jpg',
+    },
+    {
+        title: 'Greater Than Yourself',
+        src: '/images/blog-greater-than-yourself.jpg',
+    },
+    {
+        title: 'Janesberry Person Node',
+        src: '/images/janesberry-person-graph-node.jpg',
+    },
+    {
+        title: 'UX Honeycomb',
+        src: '/images/blog-ux-honeycomb.jpg',
+    },
+    {
+        title: 'Design Thinking Ladder',
+        src: '/images/blog-design-thinking-ladder.jpg',
+    },
+    {
+        title: 'Green Marketing Pie',
+        src: '/images/blog-green-marketing.jpg',
+    },
+    {
+        title: 'Janesberry Short Term Goals',
+        src: '/images/janesberry-short-term-goals.jpg',
+    },
+    {
+        title: 'Think, Concept, Build',
+        src: '/images/blog-think-concept-build.jpg',
+    },
+    {
+        title: 'Stage Gate',
+        src: '/images/blog-stage-gate.jpg',
+    },
+    {
+        title: 'Tao of Communications',
+        src: '/images/blog-tao-communications.jpg',
+    },
+    {
+        title: 'Innovator Traits',
+        src: '/images/blog-innovator-traits.jpg',
+    },
+]
