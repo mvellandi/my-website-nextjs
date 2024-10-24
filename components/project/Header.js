d93206a0f08982dd2c6184a4eeb223aaa6d3bda4
Mario Vellandi <mvellandi@hotmail.com>
2024-10-24 21:54:26 +0200

    ~ add 0 bottom margin to project name

--- components/project/Header.js
+++ components/project/Header.js
@@ -1,32 +1,32 @@
-import cn from "classnames";
+import cn from 'classnames'
 
 export default function Header({ name, sector, coverImage }) {
-  function smallerText(text) {
-    return text.length > 30 ? "text-2xl" : "text-3xl";
-  }
+    function smallerText(text) {
+        return text.length > 30 ? 'text-2xl' : 'text-3xl'
+    }
 
-  const nameStyle = `${cn(
-    smallerText(name),
-    "text-balance font-light leading-[1.3] text-black -tracking-1 sm:text-3xl"
-  )}`;
+    const nameStyle = `${cn(
+        smallerText(name),
+        'mb-0 text-balance font-light leading-[1.3] text-black -tracking-1 sm:text-3xl'
+    )}`
 
-  return (
-    <header className="flex justify-between w-full gap-24 sm:justify-start sm:gap-36">
-      <div className="flex flex-col gap-8 sm:order-2">
-        <h1 className={nameStyle}>{name}</h1>
-        <p className="text-sm font-bold text-gray-700 uppercase tracking-3">
-          {sector}
-        </p>
-      </div>
-      <div className="pt-6 min-w-[100px] max-w-[120px] lg:pt-8">
-        <img
-          src={coverImage}
-          alt="altText"
-          width={120}
-          height={120}
-          className="w-[100px] h-[100px] lg:w-[120px] lg:h-[120px] order-1"
-        />
-      </div>
-    </header>
-  );
+    return (
+        <header className="flex w-full justify-between gap-24 sm:justify-start sm:gap-36">
+            <div className="flex flex-col gap-8 sm:order-2">
+                <h1 className={nameStyle}>{name}</h1>
+                <p className="text-sm font-bold uppercase tracking-3 text-gray-700">
+                    {sector}
+                </p>
+            </div>
+            <div className="min-w-[100px] max-w-[120px] pt-6 lg:pt-8">
+                <img
+                    src={coverImage}
+                    alt="altText"
+                    width={120}
+                    height={120}
+                    className="order-1 h-[100px] w-[100px] lg:h-[120px] lg:w-[120px]"
+                />
+            </div>
+        </header>
+    )
 }
