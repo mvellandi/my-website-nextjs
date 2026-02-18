5cb92081d12f22c1cae8cfb77855fac272496a55
Mario Vellandi <mvellandi@hotmail.com>
2026-02-18 14:11:34 +0100

    update

--- pages/about.tsx
+++ pages/about.tsx
@@ -11,23 +11,20 @@ export default function About({ preview = false }: AboutPageProps) {
         <PageLayout id="about" page="about">
             <h1>About Me</h1>
             <article className="prose-lg leading-[1.3] 2xl:leading-[1.4]">
-                <h4>Resourceful and helpful to others.</h4>
-                <p className="mb-10">
-                    I&apos;m a multiclass adventurer bringing marketing,
-                    software engineering, and media production to the table. I
-                    would like to better serve customers through communications
-                    and product development. In addition to a brief CV below,
-                    you can see:
+                <p>
+                    I love helping customers. Whether it be through market
+                    research, R&D, promoting new products or just helping them
+                    out with little things. It's about consistently giving great
+                    service and real value. I learned this from my parents who
+                    ran a motel by Disneyland, and I've tackled whatever needed
+                    to be done to serve customers exceptionally well.
+                </p>
+                <p>
+                    That's included product manufacturers, media, nonprofits, a
+                    game writer, and a computer vision company.
+                    <br /> How can I help you?
                 </p>
                 <ul>
-                    <li>
-                        <Link
-                            href="https://www.youtube.com/watch?v=lqUNdlZV0yQ"
-                            target="_blank"
-                        >
-                            My story in video (7min)
-                        </Link>
-                    </li>
                     <li>
                         <Link
                             href="https://www.linkedin.com/in/mvellandi/"
@@ -45,78 +42,15 @@ export default function About({ preview = false }: AboutPageProps) {
                             Skill Chart
                         </Link>
                     </li>
+                    <li>
+                        <Link
+                            href="https://www.youtube.com/watch?v=xXLwITrxjkY"
+                            target="_blank"
+                        >
+                            My story in video (7min)
+                        </Link>
+                    </li>
                 </ul>
-                <p>
-                    Currently, I&apos;m building mini projects and learning AI
-                    engineering, training at{' '}
-                    <Link
-                        href="https://www.boot.dev/tracks/backend-python-typescript"
-                        target="_blank"
-                    >
-                        Boot.dev
-                    </Link>
-                    , and reading{' '}
-                    <Link
-                        href="https://charlesduhigg.com/supercommunicators/"
-                        target="_blank"
-                    >
-                        Supercommunicators
-                    </Link>
-                </p>
-                <h2>CV</h2>
-                <h3>Upbringing</h3>
-                <p>
-                    - Raised in LA by hotel and packaging entrepreneurs
-                    <br />
-                    - Spent many summers in Germany
-                    <br />- Exchange student to Sweden
-                </p>
-                <h3>Young Adult</h3>
-                <p>
-                    - Sold helmets to skate shops nationwide
-                    <br />
-                    - Did customer service and operations jobs
-                    <br />
-                    - Created websites for student financial aid, work, and
-                    local volunteering
-                    <br />
-                    - U.S. Congress scholarship to study/work in Germany
-                    <br />
-                    - Intern at 2 ocean shipping companies
-                    <br />
-                    - Bachelors International Business
-                    <br />- Global Logistics Specialist certification
-                </p>
-                <h3>Early Career</h3>
-                <p>
-                    - Marketing for home goods manufacturers
-                    <br />
-                    - Created websites & media for clients
-                    <br />
-                    - Guest speaker on social marketing
-                    <br />- Community and content for B2B media
-                </p>
-                <h3>Professional Career</h3>
-                <p>
-                    - Moved to New York City
-                    <br />
-                    - CRM, UX, and communications manager
-                    <br />
-                    - Content strategy advisor to brands
-                    <br />
-                    - Meetup organizer for UX and media
-                    <br />
-                    - Moved to Bremen, Germany
-                    <br />
-                    - Public markets investor
-                    <br />
-                    - Built projects with JS, Elixir, and Figma
-                    <br />
-                    - 4mo. software training with Elixir
-                    <br />
-                    - Began backend training at Boot.dev
-                    <br />- Video project for tech education
-                </p>
             </article>
         </PageLayout>
     )
